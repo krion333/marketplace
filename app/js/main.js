30a31624675295dfc3ef2d2d2d0e6901d1c23b2b
krion333 <avesta2012@rambler.ru>
2020-05-12 18:52:46 +0400

    added category page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,58 @@
 $(function(){
 
+   $(".ratestar").rateYo({
+        rating: 4.5,
+        starWidth: "17px",
+        readOnly: true,
+   });
+
+
+   $('.featured__slider-items').slick({
+       
+        nextArrow: '<button class="slick-arrow slick-next"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"></button>',
+      
+   });
+
+   $('.followers__slider-items').slick({
+     slidesToShow: 3,
+     slidesToScroll: 1,
+     infinite: false,
+        nextArrow: '<button class="slick-arrow slick-next"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"></button>',
+      
+   });
+
+   $('.release__filter').on('click', function () {
+       $('.release__filter-category').css({ "display": "block"}); 
+               
+   });  
+
+         
+    $('.page').on('click', function () {
+          $('.category').slideToggle();
+     
+ }); 
+
+ $('.filt').on('click', function () {
+     $('.product').slideToggle();
+
+}); 
+$('.pric').on('click', function () {
+     $('.form').slideToggle();
+
+}); 
+
+ $(".js-range-slider").ionRangeSlider({
+     type: "double",
+     min: 0,
+     max: 1000,
+     from: 0,
+     to: 300,
+     prefix: "$",
+    
+ });
+
+   var mixer = mixitup('.release__inner-box');
+   
 });
